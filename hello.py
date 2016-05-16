0ea6305cac1418f0c0487c830344e4dc3d4c92c2
yanni-zhang <524244172@qq.com>
2016-05-16 14:13:36 +0800

    add database

--- hello.py
+++ hello.py
@@ -1,48 +1,95 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+import os, pymysql
+from flask.ext.migrate import Migrate, MigrateCommand
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique = True)
+	users = db.relationship('User', backref = 'role', lazy = 'dynamic')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(64), unique =True, index = True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
 
 class NameForm(Form):
 	name = StringField("你的名字：", validators = [Required()])
 	submit = SubmitField("提交")
+
+
+#app，db，表，集成到Python Shell
+def make_shell_context():
+	return dict(app = app, db = db, User = User, Role = Role)
+manager.add_command("shell", Shell(make_context = make_shell_context))
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
 	form = NameForm()
 	# data能被validators的所有验证函数接受，下面这个方法就是True
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like you have changed your name!')
+		#查询该用户是否已经在数据库记录
+		user = User.query.filter_by(username = form.name.data).first()
+		if user == None:
+			user = User(username = form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
 		#将表单name的data保存在session中，并重定向以解决POST请求后刷新的警告
 		session['name'] = form.name.data
+		form.name.data = ''
 		return redirect(url_for('index'))
 	return render_template('index.html', current_time = datetime.utcnow(), 
-		form = form, name = session.get('name'))
+		form = form, name = session.get('name'), known = session.get('known', False))
+
 
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name = name)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
+
 	return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
 	manager.run()
