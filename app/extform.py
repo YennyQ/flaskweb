d2beb6a4c68a507bd6b2b1595428fff669bde156
yanni-zhang <524244172@qq.com>
2016-06-07 22:22:55 +0800

    beautified

--- app/extform.py
+++ app/extform.py
@@ -7,7 +7,7 @@ class TagListField(Field):
 	widget = TextInput()
 
 	def __init__(self, label=None, validators=None, **kwargs):
-		super().__init__(label, validators, **kwargs)
+		super(TagListField, self).__init__(label, validators, **kwargs)
 
 	def _value(self):
 		if self.data:
